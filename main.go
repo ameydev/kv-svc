60bdf45ee618029d3fee4ee22ea75cea7c31d75c
ameydev <ameydev2@gmail.com>
2022-01-04 20:02:16 +0530

    Added prometheus metrics for - Requests status counts for each endpoint - Count of keys in the DB - Histogram of http request durations

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 // Entity - struct for all entities
@@ -20,9 +23,45 @@ type Entity struct {
 }
 
 var Entities []Entity
+var (
+	keyConter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "entity_counter",
+		Help: "total no. of keys in the DB",
+	})
+)
+
+var getCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_request_get_count", // metric name
+		Help: "Number of get_key request.",
+	},
+	[]string{"status"}, // labels
+)
+
+var postCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_request_post_count", // metric name
+		Help: "Number of post_key request.",
+	},
+	[]string{"status"}, // labels
+)
+
+var searchCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_request_search_count", // metric name
+		Help: "Number of search_key request.",
+	},
+	[]string{"status"}, // labels
+)
+
+var (
+	httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "http_duration_seconds",
+		Help: "Duration of HTTP requests.",
+	}, []string{"path"})
+)
 
 func getall(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: getall")
 	json.NewEncoder(w).Encode(Entities)
 }
 
@@ -38,10 +77,12 @@ func returnSingleEntityValue(w http.ResponseWriter, r *http.Request) {
 	}
 	if keyFound {
 		w.WriteHeader(http.StatusOK)
+		getCounter.WithLabelValues("200").Inc()
 	} else {
 		log.Println("Get request: invalid key")
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("404 - No data found!"))
+		getCounter.WithLabelValues("404").Inc()
 	}
 }
 
@@ -56,14 +97,14 @@ func createNewEntity(w http.ResponseWriter, r *http.Request) {
 		log.Println("Post request: Invalid request body")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("400 - Bad request!"))
+		postCounter.WithLabelValues("400").Inc()
 	} else {
 		Entities = append(Entities, entity)
 		json.NewEncoder(w).Encode(entity)
 		w.WriteHeader(http.StatusAccepted)
+		keyConter.Inc()
+		postCounter.WithLabelValues("200").Inc()
 	}
-
-	// update our global Entities array to include
-	// our new Entity
 
 }
 
@@ -72,7 +113,7 @@ func search(w http.ResponseWriter, r *http.Request) {
 	var keyFound bool
 	if strings.HasPrefix(RawQuery, "prefix=") {
 		keys, ok := r.URL.Query()["prefix"]
-		// search?prefix=abc
+		// search?prefix=<prefix>
 		if !ok || len(keys[0]) < 1 {
 			log.Println("Url Param 'prefix' is missing")
 			return
@@ -90,7 +131,7 @@ func search(w http.ResponseWriter, r *http.Request) {
 
 	} else if strings.HasPrefix(RawQuery, "suffix=") {
 		keys, ok := r.URL.Query()["suffix"]
-		// /search?suffix=-1
+		// /search?suffix=<suffix>
 		if !ok || len(keys[0]) < 1 {
 			log.Println("Url Param 'suffix' is missing")
 			return
@@ -110,13 +151,16 @@ func search(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("400 - Bad Request!"))
 		log.Println("Invalid search parameters.")
+		searchCounter.WithLabelValues("400").Inc()
 		return
 	}
 	if keyFound {
 		w.WriteHeader(http.StatusOK)
+		searchCounter.WithLabelValues("200").Inc()
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("404 - No data found!"))
+		searchCounter.WithLabelValues("404").Inc()
 	}
 }
 
@@ -126,7 +170,32 @@ func handleRequests() {
 	myRouter.HandleFunc("/getall", getall)
 	myRouter.HandleFunc("/set", createNewEntity).Methods("POST")
 	myRouter.HandleFunc("/get/{key}", returnSingleEntityValue)
+	myRouter.Use(prometheusMiddleware)
+	myRouter.Path("/metrics").Handler(promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+// prometheusMiddleware implements mux.MiddlewareFunc.
+func prometheusMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		route := mux.CurrentRoute(r)
+		path, _ := route.GetPathTemplate()
+		timer := prometheus.NewTimer(httpDuration.WithLabelValues(path))
+		next.ServeHTTP(w, r)
+		timer.ObserveDuration()
+	})
+}
+
+func initMetrics() {
+	// Count the existing keys (dummy data)
+	for l, _ := range Entities {
+		log.Println("Recording existing data ", l)
+		keyConter.Inc()
+	}
+
+	prometheus.MustRegister(getCounter)
+	prometheus.MustRegister(postCounter)
+	prometheus.MustRegister(searchCounter)
 }
 
 func main() {
@@ -135,5 +204,6 @@ func main() {
 		Entity{Key: "abc-1", Value: "Thermodynamics"},
 		Entity{Key: "abc-2", Value: "Automotive Engineering"},
 	}
+	initMetrics()
 	handleRequests()
 }
